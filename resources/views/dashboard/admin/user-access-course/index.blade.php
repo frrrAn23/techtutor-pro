52387dc7f8799aa48700170532d60bd1351edb1f
zidane-sc <zidankren02@gmail.com>
2023-11-23 15:34:19 +0700

    fix varible not exist

--- resources/views/dashboard/admin/user-access-course/index.blade.php
+++ resources/views/dashboard/admin/user-access-course/index.blade.php
@@ -77,31 +77,6 @@
         </div>
     </div>
 </div>
-
-@foreach ($categories as $category)
-<div class="modal fade list-courses-{{ $category->id }}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">List kursus</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <ul>
-                    @forelse ($category->courses as $course)
-                        <li>{{ $course->name }}</li>
-                    @empty
-                        <li>Tidak ada kursus</li>
-                    @endforelse
-                </ul>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-            </div>
-        </div>
-    </div>
-</div>
-@endforeach
 @endsection
 
 @push('script')
